0d1fb2b1121e2b6be3682384c129886282c534cc
aeedaa <200103275@stu.sdu.edu.kz>
2022-11-10 22:00:59 +0600

    created button and logo

---
+++ app/src/main/java/com/example/projectm/LoginScreen.kt
@@ -0,0 +1,15 @@
+package com.example.projectm
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+
+class LoginScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_screen)
+
+    }
+}
